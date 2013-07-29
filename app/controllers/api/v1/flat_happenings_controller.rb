2461d761385a76349ecfc7f938dd7e6029ea20a5
Ed Tewiah <etewiah@hotmail.cim>
2013-07-29 09:34:11 +0200

    Check for duplicate happening when creating

--- app/controllers/api/v1/flat_happenings_controller.rb
+++ app/controllers/api/v1/flat_happenings_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::FlatHappeningsController < ApplicationController
   # GET /flat_happenings.json
   def index
     if params[:city]
-      city = params[:city]
+      city = params[:city].downcase
     else
-      city = "Madrid"
+      city = "madrid"
     end
 
     if params[:day_range]
@@ -35,7 +35,8 @@ class Api::V1::FlatHappeningsController < ApplicationController
   # POST /flat_happenings
   # POST /flat_happenings.json
   def create
-    @flat_happening = FlatHappening.new(params[:flat_happening])
+    @flat_happening = FlatHappening.where(:namespaced_id => params[:flat_happening][:namespaced_id]).first_or_initialize(params[:flat_happening])
+    # @happening = Happening.where(:source => params[:happening][:source], :meta => params[:happening][:meta]).first_or_initialize(create_params)
 
     if @flat_happening.save
       render json: @flat_happening, status: :created
--- app/models/flat_happening.rb
+++ app/models/flat_happening.rb
@@ -1,4 +1,5 @@
 class FlatHappening < ActiveRecord::Base
+	before_save :lowercase
   attr_accessible :address, :cancelled, :city, :classification, :country, :description_html, :description_text, :end_time, :images, :language, :latitude, :longitude, :meta, :name, :people, :postal_code, :recurring, :recurring_details, :region, :start_time, :tags, :tickets, :timezone, :urls, :venue, :namespaced_id
 
 	serialize :images, JSON
@@ -10,4 +11,13 @@ class FlatHappening < ActiveRecord::Base
 	validates :name, :presence => true
 	validates :city, :presence => true
 	validates :country, :presence => true
+
+	def lowercase
+  	if self.city?
+	  	self.city.downcase!
+	  end
+	  if self.country?
+	  	self.country.downcase!
+	  end
+  end
 end
